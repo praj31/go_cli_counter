90a84566c0956fe568451a3e3b357e86ddaa45d4
praj31 <rajputpraj.31@gmail.com>
2022-05-27 10:12:45 +0530

    Refactored context variable in service.go

--- pkg/cache/service.go
+++ pkg/cache/service.go
@@ -6,8 +6,10 @@ import (
 	"log"
 )
 
+var ctx = context.Background()
+
 func (r *RedisClient) ListCounters() {
-	res, err := r.cache.HKeys(context.Background(), "counter").Result()
+	res, err := r.cache.HKeys(ctx, "counter").Result()
 	if err != nil {
 		log.Fatal("[ERR] No \"counter list\" found. Are you sure your counters list is not empty?")
 	}
@@ -22,43 +24,43 @@ func (r *RedisClient) ListCounters() {
 }
 
 func (r *RedisClient) AddCounter(name string) {
-	exists, _ := r.cache.HExists(context.Background(), "counter", name).Result()
+	exists, _ := r.cache.HExists(ctx, "counter", name).Result()
 	if exists {
 		log.Fatalf("[ERR] A counter named %v already exists!", name)
 	}
-	r.cache.HSet(context.Background(), "counter", name, 0)
+	r.cache.HSet(ctx, "counter", name, 0)
 	fmt.Printf("Added Counter: %v\n", name)
 }
 
 func (r *RedisClient) RemoveCounter(name string) {
-	exists, _ := r.cache.HExists(context.Background(), "counter", name).Result()
+	exists, _ := r.cache.HExists(ctx, "counter", name).Result()
 	if !exists {
 		log.Fatalf("[ERR] Could not find any counter having key name \"%v\". Please check if the counter exists by running `counter list`.", name)
 	}
-	r.cache.HDel(context.Background(), "counter", name)
+	r.cache.HDel(ctx, "counter", name)
 	fmt.Printf("Removed Counter: %v\n", name)
 }
 
 func (r *RedisClient) IncrementCounter(name string) {
-	exists, _ := r.cache.HExists(context.Background(), "counter", name).Result()
+	exists, _ := r.cache.HExists(ctx, "counter", name).Result()
 	if !exists {
 		log.Fatalf("[ERR] Could not find any counter having key name \"%v\". Please check if the counter exists by running `counter list`.", name)
 	}
-	r.cache.HIncrBy(context.Background(), "counter", name, 1)
+	r.cache.HIncrBy(ctx, "counter", name, 1)
 	fmt.Printf("Increment Counter by 1: %v\n", name)
 }
 
 func (r *RedisClient) DecrementCounter(name string) {
-	exists, _ := r.cache.HExists(context.Background(), "counter", name).Result()
+	exists, _ := r.cache.HExists(ctx, "counter", name).Result()
 	if !exists {
 		log.Fatalf("[ERR] Could not find any counter having key name \"%v\". Please check if the counter exists by running `counter list`.", name)
 	}
-	r.cache.HIncrBy(context.Background(), "counter", name, -1)
+	r.cache.HIncrBy(ctx, "counter", name, -1)
 	fmt.Printf("Decrement Counter by 1: %v\n", name)
 }
 
 func (r *RedisClient) GetCounter(name string) {
-	res, err := r.cache.HGet(context.Background(), "counter", name).Result()
+	res, err := r.cache.HGet(ctx, "counter", name).Result()
 	if err != nil {
 		log.Fatalf("[ERR] Could not find any counter having key name \"%v\". Please check if the counter exists by running `counter list`.", name)
 	}
@@ -66,10 +68,10 @@ func (r *RedisClient) GetCounter(name string) {
 }
 
 func (r *RedisClient) SetCounter(name string, n int) {
-	exists, _ := r.cache.HExists(context.Background(), "counter", name).Result()
+	exists, _ := r.cache.HExists(ctx, "counter", name).Result()
 	if !exists {
 		log.Fatalf("[ERR] Could not find any counter having key name \"%v\". Please check if the counter exists by running `counter list`.", name)
 	}
-	r.cache.HSet(context.Background(), "counter", name, n)
+	r.cache.HSet(ctx, "counter", name, n)
 	fmt.Printf("Set Counter to %v: %v\n", n, name)
 }
